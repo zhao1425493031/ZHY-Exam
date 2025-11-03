9828542626b08ab6d93ab08d3eb7626e77a96641
赵宏宇 <zhaohongyu_mic@outlook.com>
2025-11-03 15:31:49 +0900

    Update AdminDashboard.vue

--- frontend/src/views/admin/AdminDashboard.vue
+++ frontend/src/views/admin/AdminDashboard.vue
@@ -710,6 +710,10 @@ export default {
         background: linear-gradient(135deg, #f093fb 0%, #f5576c 100%);
       }
       
+      &.user-subjects {
+        background: linear-gradient(135deg, #667eea 0%, #764ba2 100%);
+      }
+      
       &.questions {
         background: linear-gradient(135deg, #4facfe 0%, #00f2fe 100%);
       }
